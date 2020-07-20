23275f9da61d23fb2f7186935353462d7a930f6c
Steven Michael <ssmichael@gmail.com>
2020-07-20 12:21:51 -0400

    add compiled seirmodel

--- App.js
+++ App.js
@@ -14,39 +14,13 @@ dbService.connect(err => {
 
     // Some definitions for static files
     app.get('/', function (req, res) { res.sendFile(path.join(__dirname + '/index.html')) })
-    app.get('/sierplot.js', function (req, res) { res.sendFile(path.join(__dirname + '/seirplot.js')) })
     app.get('/favicon.ico', function (req, res) { res.sendFile(path.join(__dirname + "/favicon.ico")) })
 
     // Custom javascript files
     app.use('/customjs', express.static('customjs'))
     app.use('/static', express.static('static'))
     app.use('/data', express.static('data'))
-    // node.js files
-    // app.use('/js', express.static('node_modules'))
 
-    /////////////////////////////////////////////////////////////
-    // Database Queries
-    app.get('/data/state/:statename', function (req, res) {
-        dbService.state_data(req.params.statename, result => {
-            res.send(result)
-        })
-    })
-    app.get('/data/country/:country', function (req, res) {
-        dbService.country_data(req.params.country, result => {
-            res.send(result)
-        })
-    })
-    app.get('/data/statelist', function (req, res) {
-        dbService.statelist(result => {
-            res.send(result)
-        })
-    })
-    app.get('/data/countrylist', function (req, res) {
-        dbService.countrylist(result => {
-            res.send(result)
-        })
-    })
     //////////////////////////////////////////////////////////
-
     app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 })
