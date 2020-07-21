1828bf7717785cb994201ca6660d6cf67ffa930f
Steven Michael <ssmichael@gmail.com>
2020-07-21 15:41:41 -0400

    adopt new normal

--- src/module.cpp
+++ src/module.cpp
@@ -24,13 +24,17 @@ ResultsType testfunc(const val &v)
 
     seir.R0Table_.clear();
     double R0 = v["R0"].as<double>();
-    double intervention_r0 = R0 * (1.0 - v["R0_reduction"].as<double>());
     double intervention_time = v["Tintervention"].as<double>();
-    double intervention_end =
-        intervention_time + v["intervention_duration"].as<double>();
+    double intervention_duration = v["intervention_duration"].as<double>();
     seir.R0Table_.push_back(R0TableElement(R0, 0));
-    seir.R0Table_.push_back(R0TableElement(intervention_r0, intervention_time));
-    seir.R0Table_.push_back(R0TableElement(R0, intervention_end));
+
+    if (intervention_duration) {
+        double intervention_end = intervention_duration + intervention_time;
+        seir.R0Table_.push_back(R0TableElement(
+            v["R0_intervention"].as<double>(), intervention_time));
+        seir.R0Table_.push_back(
+            R0TableElement(v["R0_newnormal"].as<double>(), intervention_end));
+    }
 
     seir.Hc_ = v["Hc"].as<double>();
     seir.pfat_increase_nohospital_ = v["pfat_increase_nohos"].as<double>();
